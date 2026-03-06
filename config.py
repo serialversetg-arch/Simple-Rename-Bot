b6525e5b57ed9b7a43260633f25e2b78adc26d5a
serialversetg-arch <serialversetg@gmail.com>
2026-03-06 12:17:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "26910777"))
 API_HASH = environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", "5232142502"))          
-CAPTION = environ.get("CAPTION", "{file_name}")
+DB_URL = environ.get("DB_URL", "mongodb+srv://King:Cobra765592@cluster0.qy4m5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # for thumbnail ( back end is MrAniket brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
