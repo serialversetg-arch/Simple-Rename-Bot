a3e0b1f98a6ff96af49f15ed157e54c1e2482c48
serialversetg-arch <serialversetg@gmail.com>
2026-03-06 11:50:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "26910777"))
 API_HASH = environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6661964716:AAGvRVky80T46iSJLJiXgtijYKC5wMhSJJw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", "5232142502"))          
 CAPTION = environ.get("CAPTION", "{file_name}")
 
